eb62c08bde8c8c84d864752dbe399c15053d5992
denis voloshko <voloshko@gmail.com>
2026-02-13 21:03:15 +0200

    formatter haskell fixed

--- nvim/.config/nvim/lua/plugins/haskell.lua
+++ nvim/.config/nvim/lua/plugins/haskell.lua
@@ -4,6 +4,12 @@ return {
     opts = {
       formatters_by_ft = {
         haskell = { "ormolu" },
+        cabal = {},
+      },
+      formatters = {
+        ormolu = {
+          command = vim.fn.expand("~/.cabal/bin/ormolu"),
+        },
       },
     },
   },
